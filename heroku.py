c55f1b1c42f219ca3f7367f3dcd4eab8fa9ed4e3
Common_sensei <53259730+RobiMez@users.noreply.github.com>
2020-10-16 12:53:56 +0300

    init commit to heroku testing webhooks

---
+++ heroku.py
@@ -0,0 +1,19 @@
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+TOKEN = "1086396783:AAFl7mW9mkuYnmwenRePOW-RSR2AOzurC20"
+PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN)
+# add handlers
+
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Welcome dude')
+
+
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://emvc-bot.herokuapp.com/" + TOKEN)
+updater.idle()
