6fbfb5fab92ab1dc746afbcefc4956d18496b89c
Common_sensei <53259730+RobiMez@users.noreply.github.com>
2020-10-16 13:55:08 +0300

    adding github actions to auto deploy

--- heroku.py
+++ heroku.py
@@ -4,20 +4,32 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 TOKEN = "1086396783:AAFl7mW9mkuYnmwenRePOW-RSR2AOzurC20"
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
+
+print('----------- - Bot Alive - ----------')
 # add handlers
-print('hey im alive')
 
 
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Welcome dude')
+    update.message.reply_text(
+        'Welcome dude \nsend /help if you are not sure what to do ')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text(
+        'send art here and ill send it to admins for them to verify and we will post it to the emvc channel to get you some recognition kay .')
 
 
 dp = updater.dispatcher
 dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("help", help))
 
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
+
+# ----------------------------------------------------------------
+# webhook shit
+# do not touch
+# unless you know what you doing
+updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 updater.bot.set_webhook("https://emvc-bot.herokuapp.com/" + TOKEN)
 updater.idle()
