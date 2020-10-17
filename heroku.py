580992672c33207a028413a73619b0408c7c96b2
Common_sensei <53259730+RobiMez@users.noreply.github.com>
2020-10-17 11:09:40 +0300

    adding into live

--- heroku.py
+++ heroku.py
@@ -35,89 +35,105 @@ print('\n----------- - ✔️  Bot is Alive - ----------\n')
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text(
-        'Hey There bud , Im the EMVC bot \n send me your art so that i can post them to the EMVC channel \n send /help for how to use me ')
+        'Welcome dude \nSend /help if you are not sure what to do ')
 
 
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text(
-        'Send me your art here and i will send it to the admins for them to verify\n and then we will post it to the Emvc channel to get you some recognition, adding the description function soon.')
-
-
-def todo(update, context):
-    """Send a message when the command /todo is issued."""
-    update.message.reply_text(
-        """ 
-Features to add will be 
-- The Description should be user input
-- The user link should have "@" in-front of it.
-- We also have an art category, I'd like u to test out a button way of posting the art category. I.e, when the art is posted on the channel below the description there should be an art category that are hardcoded hashtags like #environment, #character, #vfx and so on.
-So the flow of input should be more like.
-
-User sends Art -> Bot asks what the description of the art is -> User types the description of the art -> Bot gives a list of buttons to pick the type of art posted -> Bot sends a message that the art is sent to the admins waiting for approval.
-
-
-Bot reaches to admin group and posts the art with the users link so we can contact him to make changes , there should be an accept and reject option. A rejection will send the user that his art is rejected. The accept button will do what it does now to the channel, with the added features mentioned above. 
-
-The rest is absolutely perfect.""")
+        'Send art here and i will send it to admins for them to verify and we will post it to the Emvc channel to get you some recognition kay .')
 
 
 def artHandler(update, context):
-    """desc 
+    """handle art from the bot 
+    take the art from the bot 
+    send it to the admins group that is linked to the final channel 
     """
 
     message = update.effective_message
-    print(message)
     file_id = message.photo[0].file_unique_id
+    print("\n------------ artHandler message ----------\n")
+    print(message)
+    print("\n------------ artHandler file_id ----------\n")
     print(file_id)
+    print("\n------------------------------------------\n")
     keyboard = InlineKeyboardMarkup([{
         InlineKeyboardButton(
-            "verify art ", callback_data=f"verify_art({file_id})")
+            "Accept", callback_data=f"accept_art({file_id})")
+    }, {
+        InlineKeyboardButton(
+            "Reject", callback_data=f"reject_art({file_id})")
     }])
 
-    context.bot.send_photo(chat_id=group_chat_id, photo=message.photo[0].file_id, caption=f"""
---- Bot generated verification message ---
+    context.bot.send_photo(
+        chat_id=group_chat_id,
+        photo=message.photo[0].file_id,
+        caption=f"""
 Artist : {message.chat.first_name}
 Username : {message.chat.username}
-""", parse_mode=ParseMode.MARKDOWN, reply_markup=keyboard)
+""",    parse_mode=ParseMode.MARKDOWN,
+        reply_markup=keyboard)
 
 
-def verify_button(update, context):
-    """Handles the verification process
+def accept_button(update, context):
+    """handles the art accept button 
     """
 
     message = update.effective_message
     data = update.callback_query
+    user_data = data.from_user
     file_id = data['message']['photo'][0].file_id
-    print("----------------------")
-    print(message)
-    print("----------------------")
-    print(data)
-    print("----------------------")
-    print(file_id)
-    print("----------------------")
 
+    print("\n----------- accept_button message  ----------\n")
+    print(message)
+    print("\n----------- accept_button user_data ---------\n")
+    print(user_data)
+    print("\n----------- accept_button file_id  ----------\n")
+    print(file_id)
+    print("\n---------------------------------------------\n")
+    context.bot.send_message(
+        chat_id=user_data.id, text="your art has been accepted and is posted to the channel")
     context.bot.send_photo(chat_id=channel_id, photo=file_id, caption=f"""
-Artist : {message.chat.first_name}
-Username : {message.chat.username}
-Description : lorem ipsum sir dolor amet
-Tags : 
+Artist : {user_data.first_name} {user_data.last_name}
+Username : @{user_data.username}
 """)
+
+
+def reject_button(update, context):
+    """handles the art reject button 
+    """
+
+    message = update.effective_message
+    data = update.callback_query
+    user_data = data.from_user
+    file_id = data['message']['photo'][0].file_id
+
+    print("\n----------- reject_art message  ----------\n")
+    print(message)
+    print("\n----------- reject_art user_data ---------\n")
+    print(user_data)
+    print("\n----------- reject_art file_id  ----------\n")
+    print(file_id)
+    print("\n---------------------------------------------\n")
+
+    context.bot.send_message(
+        chat_id=user_data.id, text="Sorry but your art has been rejected ")
 
 
 # ----------------------------------------------------------------
 # Dispatchers and Handlers go here
-CALLBACK_QUERY_HANDLER = CallbackQueryHandler(
-    verify_button, pattern=r"verify_art")
+ACCEPT_CALLBACK_QUERY_HANDLER = CallbackQueryHandler(
+    accept_button, pattern=r"accept_art")
+REJECT_CALLBACK_QUERY_HANDLER = CallbackQueryHandler(
+    reject_button, pattern=r"reject_art")
 
 dp = updater.dispatcher  # Declaring the dispatcher
 
 dp.add_handler(CommandHandler("start", start))
 dp.add_handler(CommandHandler("help", help))
-dp.add_handler(CommandHandler("todo", todo))
 dp.add_handler(MessageHandler(Filters.photo, artHandler))
-dp.add_handler(CALLBACK_QUERY_HANDLER)
-
+dp.add_handler(ACCEPT_CALLBACK_QUERY_HANDLER)
+dp.add_handler(REJECT_CALLBACK_QUERY_HANDLER)
 
 # ----------------------------------------------------------------
 # Webhook stuff
